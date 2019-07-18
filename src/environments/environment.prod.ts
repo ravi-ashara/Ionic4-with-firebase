40dbaacffa5974e84dd1c6fcbb6da4a46333894a
ravi-ashara <raviashar94@gmail.com>
2019-07-18 09:33:39 +0530

    Update Code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCIBPuZK_XqaWzEtlUuK3gwRE7wuLGEAtQ",
-    authDomain: "caramel-box-109312.firebaseapp.com",
-    databaseURL: "https://caramel-box-109312.firebaseio.com",
-    projectId: "caramel-box-109312",
-    storageBucket: "caramel-box-109312.appspot.com",
-    messagingSenderId: "547124143852"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
